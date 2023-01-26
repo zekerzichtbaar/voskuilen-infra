f882e0fae3001ca81dd40bdc8e082b059abb7321
ThijsBronsNL <thijsbronsnl@gmail.com>
2023-01-26 17:10:43 +0100

    Fix

---
+++ web/app/cache/acorn/framework/views/c3c3b93da32aa58ee0f9d637e385594fe0ff12c7.php
@@ -0,0 +1,5 @@
+<div <?php echo e($attributes->merge(['class' => $type])); ?>>
+  <?php echo $message ?? $slot; ?>
+
+</div>
+<?php /**PATH /Users/thijsbrons/Sites/voskuilen-infra/web/app/themes/voskuilen-infra/resources/views/components/alert.blade.php ENDPATH**/ ?>
--- web/app/themes/voskuilen-infra/resources/views/sections/header.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/sections/header.blade.php
@@ -1,11 +1,24 @@
 <header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
-  </a>
+  <div class="container text-white">
+    <div class="flex justify-between items-center">
+      <a class="brand" href="{{ home_url('/') }}">
+        <img src="{{ asset('images/logo.svg') }}">
+      </a>
 
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
+      @if (has_nav_menu('primary_navigation'))
+        <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex gap-16 font-semibold', 'echo' => false]) !!}
+        </nav>
+      @endif
+
+      <div class="flex justify-center items-center gap-4 border border-white px-8 py-6">
+        <span class="font-black uppercase">Menu</span>
+        <div class="flex flex-col gap-1">
+          <div class="w-6 h-[3px] bg-white"></div>
+          <div class="w-6 h-[3px] bg-white"></div>
+          <div class="w-6 h-[3px] bg-white"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </header>
