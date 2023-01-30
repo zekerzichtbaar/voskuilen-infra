3dfbf76aa0ad0850734856d4144ce1e18854a250
ThijsBronsNL <thijsbronsnl@gmail.com>
2023-01-30 11:50:03 +0100

    cache

--- web/app/cache/acorn/framework/views/1d9e11ae6cb1f765254f173a33027b8c3a660ddf.php
+++ web/app/cache/acorn/framework/views/1d9e11ae6cb1f765254f173a33027b8c3a660ddf.php
@@ -1,21 +1,19 @@
-<section class="relative border-[30px] border-white">
-        <div class="<?php echo e($type = "header" ? "pt-52" : ""); ?>">
+<section class="relative border-[30px] border-white py-0">
+    <?php if($type == "header"): ?>
+        <div class="text-white" style="height: 80vh;">
             <?php if($background = "bg_image"): ?>
                 <?php if($bg_image): ?><?php echo wp_get_attachment_image( $bg_image['ID'], isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ); ?><?php endif; ?>
-                <div class="absolute inset-x-0 bg-gradient-to-t from-black/70 to-transparent h-1/2 bottom-0 z-10"></div>
-                <div class="absolute inset-x-0 bg-gradient-to-b from-black/70 to-transparent h-1/2 top-0 z-10"></div>
+                <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
             <?php else: ?>
                 <?php if($bg_image): ?><video autoplay muted src="<?php echo e($bg_video); ?>" class="w-full h-full absolute inset-0 object-cover"</><?php endif; ?>
-                <div class="absolute inset-x-0 bg-gradient-to-t from-black/60 to-transparent h-1/2 bottom-0 z-10"></div>
-                <div class="absolute inset-x-0 bg-gradient-to-b from-black/60 to-transparent h-1/2 top-0 z-10"></div>
+                <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
             <?php endif; ?>
-        </div>
-        <div class="z-10 -bottom-[10.2rem] pl-24 relative text-white">
-            <h1><?php echo $title; ?></h1>
-            <div class="mt-20 flex gap-4">
-                <?php if($buttons): ?>
-                    <?php $__currentLoopData = $buttons; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $button): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+            <div class="container mx-auto h-full flex">
+                <h1 class="relative mt-auto mb-28"><?php echo $title; ?></h1>
+                <div class="absolute bottom-0 translate-y-1/2 text-white flex flex-wrap gap-6">
+                    <?php if($buttons): ?>
+                        <?php $__currentLoopData = $buttons; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $button): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
 <?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.button','data' => ['type' => ''.e($button['type']).'','href' => ''.e($button['link']['url']).'','target' => ''.e($button['link']['target']).'']]); ?>
 <?php $component->withName('button'); ?>
 <?php if ($component->shouldRender()): ?>
@@ -26,8 +24,18 @@
 <?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
 <?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
 <?php endif; ?>
-                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                <?php endif; ?>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
+    <?php elseif($type == "subheader"): ?>
+        <div class="text-white" style="height: 50vh;">
+            <?php if($bg_image): ?><?php echo wp_get_attachment_image( $bg_image['ID'], isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ); ?><?php endif; ?>
+            <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
+            <div class="container mx-auto h-full flex items-center">
+                <h1 class="relative"><?php echo $title; ?></h1>
+            </div>
+        </div>
+    <?php endif; ?>
 </section><?php /**PATH /Users/thijsbrons/Sites/voskuilen-infra/web/app/themes/voskuilen-infra/resources/views/blocks/header.blade.php ENDPATH**/ ?>
--- web/app/cache/acorn/framework/views/ec1f8eb69bb695c73312096cd6dbba4395124982.php
+++ web/app/cache/acorn/framework/views/ec1f8eb69bb695c73312096cd6dbba4395124982.php
@@ -1,19 +1,19 @@
-<header class="banner fixed top-0 z-40 w-full">
-  <div class="max-w-[95vw] md:max-w-[80vw] mx-auto text-white">
-    <div class="flex justify-between items-center md:items-end">
-      <a class="brand" href="<?php echo e(home_url('/')); ?>">
+<header class="banner fixed top-0 z-40 w-full border-x-[30px] border-white">
+  <div class="container mx-auto text-white">
+    <div class="flex justify-between items-center pt-[50px]">
+      <a class="brand -mt-[50px]" href="<?php echo e(home_url('/')); ?>">
         <img class="w-20 md:w-auto h-auto" src="<?php echo e(asset('images/logo.svg')); ?>">
       </a>
 
       <?php if(has_nav_menu('primary_navigation')): ?>
-        <nav class="nav-primary hidden md:flex" aria-label="<?php echo e(wp_get_nav_menu_name('primary_navigation')); ?>">
+        <nav class="nav-primary hidden lg:flex" aria-label="<?php echo e(wp_get_nav_menu_name('primary_navigation')); ?>">
           <?php echo wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex gap-16 font-semibold', 'echo' => false]); ?>
 
         </nav>
       <?php endif; ?>
 
       <div id="menu" class="hamburger flex justify-center items-center gap-4 border border-white px-6 py-3 md:px-8 md:py-6 cursor-pointer transition duration-300 ease-in-out">
-        <span class="font-black uppercase">Menu</span>
+        <span class="font-black uppercase h-5">Menu</span>
         <div id="hamburger" class="flex">
           <div class="icon-left">
             <span class="block w-4 h-[3px] transition duration-300 ease-in-out bg-white"></span>
