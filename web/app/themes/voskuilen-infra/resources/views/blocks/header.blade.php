739677ceba0ccb577e4fba7c5c25bc9c75647082
Douwe Pausma <douwepausma@gmail.com>
2023-01-30 11:35:08 +0100

    Edited container sizing

--- web/app/themes/voskuilen-infra/resources/views/blocks/header.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/blocks/header.blade.php
@@ -8,7 +8,7 @@
                 @if($bg_image)<video autoplay muted src="{{ $bg_video }}" class="w-full h-full absolute inset-0 object-cover"</>@endif
                 <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
             @endif
-            <div class="max-w-[95vw] md:max-w-[80vw] mx-auto h-full flex items-center">
+            <div class="container mx-auto h-full flex items-center">
                 <h1 class="relative">{!! $title !!}</h1>
                 <div class="absolute bottom-0 translate-y-1/2 text-white flex flex-wrap gap-6">
                     @if($buttons)
@@ -23,7 +23,7 @@
         <div class="text-white" style="height: 50vh;">
             @if($bg_image){!! wp_get_attachment_image( $bg_image['ID'], isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ) !!}@endif
             <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
-            <div class="max-w-[95vw] md:max-w-[80vw] mx-auto h-full flex items-center">
+            <div class="container mx-auto h-full flex items-center">
                 <h1 class="relative">{!! $title !!}</h1>
             </div>
         </div>
--- web/app/themes/voskuilen-infra/resources/views/sections/header.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/sections/header.blade.php
@@ -1,12 +1,12 @@
-<header class="banner fixed top-0 z-40 w-full">
-  <div class="max-w-[95vw] md:max-w-[80vw] mx-auto text-white">
-    <div class="flex justify-between items-center pt-[50px]">
-      <a class="brand -mt-[50px]" href="{{ home_url('/') }}">
+<header class="banner fixed top-0 z-40 w-full border-x-[30px] border-white">
+  <div class="container mx-auto text-white">
+    <div class="flex justify-between items-center pt-[60px]">
+      <a class="brand -mt-[60px]" href="{{ home_url('/') }}">
         <img class="w-20 md:w-auto h-auto" src="{{ asset('images/logo.svg') }}">
       </a>
 
       @if (has_nav_menu('primary_navigation'))
-        <nav class="nav-primary hidden md:flex" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        <nav class="nav-primary hidden lg:flex" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
           {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex gap-16 font-semibold', 'echo' => false]) !!}
         </nav>
       @endif
