04720cf9f6b864d04214a9d2bb6c08b4dadc9ed6
Douwe Pausma <douwepausma@gmail.com>
2023-02-02 09:30:24 +0100

    Misc changes

---
+++ web/app/themes/voskuilen-infra/resources/views/archive-project.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+
+  <div class="container py-32 grid grid-cols-2 gap-4">
+    @if (! have_posts())
+    <x-alert type="warning">
+      {!! __('Sorry, no results were found.', 'sage') !!}
+    </x-alert>
+    
+    {!! get_search_form(false) !!}
+    @endif
+    
+    @while(have_posts()) @php(the_post())
+    @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+    @endwhile
+    
+    {!! get_the_posts_navigation() !!}
+  </div>
+@endsection
+
+@section('sidebar')
+  @include('sections.sidebar')
+@endsection
---
+++ web/app/themes/voskuilen-infra/resources/views/blocks/projecten.blade.php
@@ -0,0 +1,31 @@
+<section class="relative border-[15px] md:border-[30px] border-offwhite py-0">
+    @if($type == "header")
+        <div class="text-white" style="height: 80vh;">
+            @if($background = "bg_image")
+                @if($bg_image){!! wp_get_attachment_image( $bg_image['ID'], isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ) !!}@endif
+                <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
+            @else
+                @if($bg_image)<video autoplay muted src="{{ $bg_video }}" class="w-full h-full absolute inset-0 object-cover"</>@endif
+                <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
+            @endif
+            <div class="container mx-auto h-full flex">
+                <h1 class="relative mt-auto mb-28">{!! $title !!}</h1>
+                <div class="absolute bottom-0 translate-y-1/2 text-white flex flex-wrap gap-6">
+                    @if($buttons)
+                        @foreach($buttons as $button)
+                            <x-button type="{{ $button['type'] }}" href="{{ $button['link']['url'] }}" target="{{ $button['link']['target'] }}">{!! $button['link']['title'] !!}</x-button>
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    @elseif($type == "subheader")
+        <div class="text-white" style="height: 50vh;">
+            @if($bg_image){!! wp_get_attachment_image( $bg_image['ID'], isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ) !!}@endif
+            <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
+            <div class="container mx-auto h-full flex items-center">
+                <h1 class="relative">{!! $title !!}</h1>
+            </div>
+        </div>
+    @endif
+</section>
