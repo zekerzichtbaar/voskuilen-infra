0095658825ac8a95c00bbf60a4da3e5d8090fd11
Douwe Pausma <douwepausma@gmail.com>
2023-02-06 16:22:56 +0100

    Add hover effect

--- web/app/themes/voskuilen-infra/resources/views/blocks/text-image.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/blocks/text-image.blade.php
@@ -13,8 +13,10 @@
                         <ul class="mt-12 flex flex-col gap-4">
                             @foreach($links as $link)
                                 <a href="{{ $link['item']['url'] }}">
-                                    <li class="relative flex items-baseline gap-3 border-b border-gray-200 pb-3">
-                                        <img class="w-auto h-full" src="{{ asset('images/icon-arrow-right.svg') }}" />
+                                    <li class="relative flex items-center gap-3 border-b border-gray-200 pb-3 group">
+                                        <svg class="h-4 mb-1 duration-150 text-black group-hover:translate-x-1 group-hover:text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
+                                        </svg>
                                         <span>{{ $link['item']['title'] }}</span>
                                     </li>
                                 </a>
