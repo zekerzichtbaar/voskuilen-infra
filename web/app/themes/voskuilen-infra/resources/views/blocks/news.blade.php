dc63ddc61aac923c6ee8c6e1ea1c4239fb51cb19
Douwe Pausma <douwepausma@gmail.com>
2023-02-07 13:12:13 +0100

    Update news.blade.php

--- web/app/themes/voskuilen-infra/resources/views/blocks/news.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/blocks/news.blade.php
@@ -1,11 +1,10 @@
 <section class="relative overflow-x-hidden py-32">
     <div class="container">
         <div class="swiper !overflow-visible">
-            <div class="flex flex-col md:flex-row justify-between md:items-center mb-8">
+            <div class="flex flex-col md:flex-row justify-between md:items-center border-b border-gray-200 pb-8 mb-12">
                 <{{ $heading }}>{{ $title }}</{{ $heading }}>
                 <div class="swiper-pagination"></div>
             </div>
-            <div class="block h-px w-full bg-gray-300 mb-12"></div>
             <div class="swiper-wrapper w-full">
                 @while($item = $news->have_posts()) @php($news->the_post())
                     <div class="swiper-slide">
