f3112868584fbd423e881ee50f929efb891ec48d
Douwe Pausma <douwepausma@gmail.com>
2023-02-07 13:12:44 +0100

    Frontend projects page

--- web/app/themes/voskuilen-infra/app/filters.php
+++ web/app/themes/voskuilen-infra/app/filters.php
@@ -26,3 +26,13 @@ add_filter( 'wpseo_breadcrumb_links', function($links) {
   $links[0]['text'] = '<svg class="text-primary -mt-1" height="22" width="auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/></svg>';
   return $links;
 });
+
+/**
+ * Add attributes to pagination links.
+ */
+add_filter('next_posts_link_attributes', function() {
+  return 'class="p-3 border-2 border-primary text-primary hover:text-white hover:bg-primary duration-150"';
+});
+add_filter('previous_posts_link_attributes', function() {
+  return 'class="p-3 border-2 border-primary text-primary hover:text-white hover:bg-primary duration-150"';
+});
--- web/app/themes/voskuilen-infra/resources/views/archive-project.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/archive-project.blade.php
@@ -15,23 +15,33 @@
 
   <div class="container py-20 flex gap-4">
     {!! get_search_form(false) !!}
-    <x-button type="primary">Filteren</x-button>
+    <x-button type="primary" custom-icon="true">
+      Filteren
+      <svg class="h-6 w-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"/>
+      </svg>
+    </x-button>
   </div> 
   <div class="py-32 bg-white">
-    <div class="container grid grid-cols-12 gap-4" style="grid-template-rows: repeat(auto-fit, 500px 500px 500px 500px 500px 500px)">
-      @if (! have_posts())
-        <x-alert type="warning">
-          {!! __('Sorry, no results were found.', 'sage') !!}
-        </x-alert>
-      @endif
-      @while(have_posts()) @php(the_post())
-        <div class="col-span-12 md:col-span-6">
-          @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
-        </div>
-      @endwhile
-      @php(wp_reset_postdata())
-      
-      {!! get_the_posts_navigation() !!}
+    <div class="container">
+      <div class="grid grid-cols-12 gap-4 pb-12 border-b border-b-200 mb-6" style="grid-template-rows: repeat(auto-fit, 500px 500px 500px 500px 500px 500px)">
+        @if (! have_posts())
+          <x-alert type="warning">
+            {!! __('Sorry, no results were found.', 'sage') !!}
+          </x-alert>
+        @endif
+        @while(have_posts()) @php(the_post())
+          <div class="col-span-12 md:col-span-6">
+            @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+          </div>
+        @endwhile
+        @php(wp_reset_postdata())
+      </div>
+      <div class="flex justify-between">
+        <span></span>
+        <?php next_posts_link( '<svg class="h-4 w-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/></svg>' ); ?>
+        <?php previous_posts_link( '<svg class="h-4 w-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"/></svg>' ); ?>
+      </div>
     </div>
   </div>
 @endsection
