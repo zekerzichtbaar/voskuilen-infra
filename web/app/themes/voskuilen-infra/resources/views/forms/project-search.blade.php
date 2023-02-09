8a8dcf9759fe744b68f403712d3524eeea6823d8
Douwe Pausma <douwepausma@gmail.com>
2023-02-09 11:43:54 +0100

    Update project search

--- web/app/themes/voskuilen-infra/resources/views/forms/project-search.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/forms/project-search.blade.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form w-full flex">
+<form role="search" method="get" class="search-form w-full flex relative">
   <label for="search">
     <span class="sr-only">
       {{ _x('Search for:', 'label', 'sage') }}
@@ -14,7 +14,7 @@
     name="search"
   >
 
-  <button class="px-4 bg-white text-primary">
+  <button class="px-4 bg-white text-primary absolute inset-y-1 right-1">
     <svg class="h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" ariaHidden="true">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
     </svg>
