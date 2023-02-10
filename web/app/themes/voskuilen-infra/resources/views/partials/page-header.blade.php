9a4daf4b89b16dc2d66bdcf8983958310ac49d9c
Douwe Pausma <douwepausma@gmail.com>
2023-02-10 13:55:52 +0100

    Remove bg-primary

--- web/app/themes/voskuilen-infra/resources/views/partials/page-header.blade.php
+++ web/app/themes/voskuilen-infra/resources/views/partials/page-header.blade.php
@@ -1,7 +1,7 @@
 @if(!is_front_page())
   <div class="relative text-white border-[15px] md:border-[30px] border-b-0 border-transparent" style="height: 60vh;">
     {!! wp_get_attachment_image( get_post_thumbnail_id(), isset($size), "", ["class" => "w-full h-full absolute inset-0 object-cover"] ) !!}
-    <div class="absolute inset-0 bg-primary bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-black/70 via-transparent to-black/70"></div>
     <div class="container mx-auto h-full flex items-center">
         <h1 class="relative">{!! $title !!}</h1>
     </div>
